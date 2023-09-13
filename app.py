1e9ed9000f6693840d7b9ec6d95c066be0198b82
Fabyan Kindarya <fkindarya@gmail.com>
2023-09-13 12:57:00 +0700

    Add Front End

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from timeit import default_timer as timer
 from io import BytesIO
 from ArabicOcr import arabicocr
 from datetime import datetime
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from PIL import Image
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -18,6 +19,10 @@ UPLOAD_FOLDER = 'static/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def ArabicOCR(image):
     results = arabicocr.arabic_ocr(image, image)
@@ -45,19 +50,19 @@ def QuranVerseSound(arabic_text):
 
 def CheckWordAfterNoonSaakin(arrayLength, words):
     for i in range(arrayLength):
-        if 'ن' in words[i]:
+        if '\u0646' in words[i]:
             for j in range(len(words[i])):
-                if words[i][j] == 'ن':
+                if words[i][j] == '\u0646':
                     index = i, j
                     if index[1] < len(words[i])-1:
-                        if words[index[0]][index[1]+1] == ' ':
+                        if words[index[0]][index[1]+1] == '\u0020':
                             next_word = words[index[0]][index[1]+2]
-                        elif words[index[0]][index[1]+1] != ' ':
+                        elif words[index[0]][index[1]+1] != '\u0020':
                             next_word = words[index[0]][index[1]+1]
-                            if next_word == 'ا':
-                                if words[index[0]][index[1]+2] == ' ':
+                            if next_word == '\u0627':
+                                if words[index[0]][index[1]+2] == '\u0020':
                                     next_word = words[index[0]][index[1]+3]
-                                elif words[index[0]][index[1]+2] != ' ':
+                                elif words[index[0]][index[1]+2] != '\u0020':
                                     next_word = words[index[0]][index[1]+2]
                     else:
                         next_word = 'Word Not Detected'
@@ -67,88 +72,88 @@ def CheckWordAfterNoonSaakin(arrayLength, words):
     return next_word
 
 def CheckTajweedLaws(next_word):
-    if next_word == 'أ' or next_word == 'ء':
+    if next_word == '\u0623' or next_word == '\u0621':
         law = "Idhar Halqi"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idhar/alif.mp3")
-    elif next_word == 'ح':
+    elif next_word == '\u062d':
         law = "Idhar Halqi"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idhar/kha.mp3")
-    elif next_word == 'خ':
+    elif next_word == '\u062e':
         law = "Idhar Halqi"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idhar/kho.mp3")
-    elif next_word == 'ع':
+    elif next_word == '\u0639':
         law = "Idhar Halqi"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idhar/ain.mp3")
-    elif next_word == 'غ':
+    elif next_word == '\u063a':
         law = "Idhar Halqi"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idhar/ghoin.mp3")
-    elif next_word == 'ه':
+    elif next_word == '\u0647':
         law = "Idhar Halqi"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idhar/ha.mp3")
-    elif next_word == 'ي':
+    elif next_word == '\u064a':
         law = "Idgham Bigunnah"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idgham/ya.mp3")
-    elif next_word == 'و':
+    elif next_word == '\u0648':
         law = "idgham Bigunnah"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idgham/waw.mp3")
-    elif next_word == 'م':
+    elif next_word == '\u0645':
         law = "Idgham Bigunnah"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idgham/mim.mp3")
-    elif next_word == 'ن':
+    elif next_word == '\u0646':
         law = "Idgham Bigunnah"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idgham/nun.mp3")
-    elif next_word == 'ل':
+    elif next_word == '\u0644':
         law = "Idgham Bilagunnah"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idgham/lam.mp3")
-    elif next_word == 'ر':
+    elif next_word == '\u0631':
         law = "Idgham Bilagunnah"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "idgham/ra.mp3")
-    elif next_word == 'ت':
+    elif next_word == '\u062a':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/ta.mp3")
-    elif next_word == 'ث':
+    elif next_word == '\u062b':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/tsa.mp3")
-    elif next_word == 'د':
+    elif next_word == '\u062f':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/dal.mp3")
-    elif next_word == 'ذ':
+    elif next_word == '\u0630':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/dzal.mp3")
-    elif next_word == 'ج':
+    elif next_word == '\u062c':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/jim.mp3")
-    elif next_word == 'ز':
+    elif next_word == '\u0632':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/za.mp3")
-    elif next_word == 'س':
+    elif next_word == '\u0633':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/sin.mp3")
-    elif next_word == 'ش':
+    elif next_word == '\u0634':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/syin.mp3")
-    elif next_word == 'ص':
+    elif next_word == '\u0635':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/shad.mp3")
-    elif next_word == 'ض':
+    elif next_word == '\u0636':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/dhad.mp3")
-    elif next_word == 'ط':
+    elif next_word == '\u0637':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/tha.mp3")
-    elif next_word == 'ظ':
+    elif next_word == '\u0638':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/zha.mp3")
-    elif next_word == 'ف':
+    elif next_word == '\u0641':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/fa.mp3")
-    elif next_word == 'ق':
+    elif next_word == '\u0642':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/qaf.mp3")
-    elif next_word == 'ك':
+    elif next_word == '\u0643':
         law = "Ikhfa Hakiki"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "ikhfa/kaf.mp3")
-    elif next_word == 'ب':
+    elif next_word == '\u0628':
         law = "Iqlab"
         sound = os.path.join(app.config['UPLOAD_FOLDER'], "iqlab/ba.mp3")
     else:
@@ -156,9 +161,66 @@ def CheckTajweedLaws(next_word):
         sound = "No sound"
     return law, sound
 
+# @app.route('/')
+# def hello_world():
+#     return jsonify("Hello World"),200
+
 @app.route('/')
-def hello_world():
-    return jsonify("Hello World"),200
+def index():
+    return render_template('index.html')
+
+@app.route('/upload-image-base64-web', methods=['POST'])
+def upload_image_base64_web():
+    file = request.files['path']
+    if file and allowed_file(file.filename):
+        filename = datetime.now().strftime("%d%m%Y%H%M%S") + '.' + secure_filename(file.filename).rsplit('.', 1)[1]
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # Open File
+        image = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        
+        # ArabicOCR
+        results = ArabicOCR(image)
+
+        # Get The Text After OCR
+        words = []
+        for j in range(len(results)):
+            word = results[j][1]
+            words.append(word)
+
+        # Check if ArabicOCR detected the image
+        arrayLength = len(words)
+
+        if arrayLength > 0:
+            # Turn Text Into String
+            arabic_text = ' '.join(words)
+            arabic_text = re.sub(' +', ' ', arabic_text)
+            words = []
+            words.append(arabic_text)
+            arrayLength = len(words)
+
+            # Get Verse From Quran Using Arabic Word Search
+            quran_sound = QuranVerseSound(arabic_text)
+        
+            # Check Word After Nun Sukun
+            next_word = CheckWordAfterNoonSaakin(arrayLength, words)
+
+            # Check Tajweed Laws
+            law, sound = CheckTajweedLaws(next_word)
+
+            if quran_sound == "Not Found" or quran_sound == "Max retires exceeded":
+                sound_how_to_read = sound
+            else:
+                sound_how_to_read = quran_sound
+            
+            if sound_how_to_read == "No sound":
+                display_sound = sound_how_to_read
+            elif sound_how_to_read != "Not found" and sound_how_to_read == sound:
+                display_sound = request.host_url + sound_how_to_read
+            else:
+                display_sound = sound_how_to_read
+
+            return render_template('result.html', image = request.host_url + app.config['UPLOAD_FOLDER'] + filename, arabic_text = arabic_text, word_after_noon_sakin = next_word, tajweed_law = law, sound_how_to_read = display_sound)
 
 @app.route('/upload-image-base64', methods=['POST'])
 def upload_image():
@@ -178,7 +240,7 @@ def upload_image():
     if base64_image and (image_extension in ALLOWED_EXTENSIONS):
         image_data = Image.open(BytesIO(image_decoded))
         filename = datetime.now().strftime("%d%m%Y%H%M%S") + "." + image_extension
-        image_data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename), optimize=True)
+        image_data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         # Open File
         image = os.path.join(app.config['UPLOAD_FOLDER'], filename)
